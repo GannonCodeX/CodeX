421c02e577eba2dd856b21e4b227bcfb3eff0861
CodeWithInferno <infernotech2618@gmail.com>
2025-08-22 15:54:29 -0400

    fix(sanity): configure client for dynamic data fetching

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -7,5 +7,11 @@ export const client = createClient({
   apiVersion,
   dataset,
   projectId,
-  useCdn: process.env.NODE_ENV === 'production',
-})
+  useCdn: false,
+  // These settings are recommended for Next.js >= 13.5 and Sanity client >= 3.
+  perspective: 'published',
+  stega: {
+    enabled: false,
+    studioUrl: '/admin',
+  },
+});
